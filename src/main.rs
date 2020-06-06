d7307baeecfb844c0e4dec691abbe28e2022d036
Emil Lundberg <emil@emlun.se>
2020-06-07 01:06:32 +0200

    Indent test program

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,29 @@ use leg_simulator::LegComputer;
 
 fn main() -> Result<(), String> {
     let fibonacci = "
-MOVC 0 => A
-MOVC 100 => D
-STOREP A => D
-ALU INCR D D D
-MOVC 1 => B
-STOREP B => D
+    MOVC 0 => A
+    MOVC 100 => D
+    STOREP A => D
+    ALU INCR D D D
+    MOVC 1 => B
+    STOREP B => D
 
-LOADP D => B
-ALU DECR D D D
-LOADP D => A
-ALU ADD A B C
+    LOADP D => B
+    ALU DECR D D D
+    LOADP D => A
+    ALU ADD A B C
 
-JMPR Ou ? 14
+    JMPR Ou ? 14
 
-ALU INCR D D D
-ALU INCR D D D
-STOREP C => D
+    ALU INCR D D D
+    ALU INCR D D D
+    STOREP C => D
 
-MOVC 0 => A
-ALU ECHO A A A
-JMPR Z ? -20
+    MOVC 0 => A
+    ALU ECHO A A A
+    JMPR Z ? -20
 
-HALT
+    HALT
     ";
 
     let source = fibonacci;
