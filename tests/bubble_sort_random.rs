b78e462e551c7a4f473a77bd4eda82d61f8d8e75
Emil Lundberg <emil@emlun.se>
2020-06-20 20:36:15 +0200

    Add bubble sort tests

---
+++ tests/bubble_sort_random.rs
@@ -0,0 +1,87 @@
+use leg_simulator::assemble_program;
+use leg_simulator::generate_code;
+use leg_simulator::LegComputer;
+use leg_simulator::Word;
+use rand::Rng;
+
+#[test]
+fn bubble_sort_random() -> Result<(), String> {
+    // Memory address 2, 3 contain start (inclusive), end (inclusive) of list
+    // List is sorted in place
+    let source = "
+    JMP T ? 8
+    HALT
+    HALT
+    HALT
+
+    LOAD 3 => D
+
+    LOAD 2 => C
+    ALU XOR C D => A
+    JMP Z ? 6
+
+    ALU XOR C D => A
+    JMPR Z ? 4
+    JMPR T ? 8
+    ALU DECR D D => D
+    STORE D => 3
+    JMP T ? 10
+
+    LOADP C => A
+    MOV C => B
+    ALU INCR B B => B
+    LOADP B => B
+    ALU ECHO A B => A
+    JMPR GT ? 6
+
+    ALU INCR C C => C
+    JMP T ? 16
+
+    LOADP C => A
+    ALU INCR C B => B
+    LOADP B => B
+    STOREP B => C
+    ALU INCR C C => C
+    STOREP A => C
+    JMPR T ? 4
+
+    ALU INCR C C => C
+    JMP T ? 16
+    ";
+
+    let mut program: Vec<Word> = generate_code(&assemble_program(source)?);
+
+    let start_list = 104;
+    let list_len = 8;
+    let end_list = start_list + list_len - 1;
+
+    program[2] = start_list;
+    program[3] = end_list;
+
+    let mut rng = rand::thread_rng();
+    let mut input = Vec::new();
+    while input.len() < list_len.into() {
+        input.push(rng.gen());
+    }
+
+    while program.len() < start_list.into() {
+        program.push(0);
+    }
+    for i in (0..list_len).rev() {
+        program.push(input[usize::from(i)]);
+    }
+    while program.len() < 256 {
+        program.push(0);
+    }
+
+    let computer = LegComputer::new(program).run();
+
+    input.sort();
+
+    assert_eq!(
+        *input.as_slice(),
+        computer.memory[start_list.into()..=end_list.into()]
+    );
+
+    Ok(())
+}
